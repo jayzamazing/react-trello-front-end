4d6e0f442ae8dad323959db36e94cf0557496fea
jayzamazing <adrianjaylopez@gmail.com>
2017-10-27 16:46:27 -0400

    Updated store to add redux-form. Started working on the routes for the add in index.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import Home from './components/home';
 import Boards from './components/boards';
-import Cards from './components/cards';
 import Cardslist from './components/cardslist';
 import store from './store';
 import './index.css';
@@ -10,7 +11,17 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-      <Boards />
+    <header>
+
+    </header>
+    <Router>
+      <main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/boards" component={Boards} />
+        <Route exact path="/board/:boardId" component={Cardslist} />
+      </main>
+    </Router>
   </Provider>,
 document.getElementById('root'));
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk';
 import boardReducer from './reducers/boards';
 import cardsReducer from './reducers/cards';
 import cardslistReducer from './reducers/cardslist';
+import {reducer as formReducer} from 'redux-form';
 
 export default createStore(
   combineReducers({
-    boardReducer,
-    cardsReducer,
-    cardslistReducer
+    boards: boardReducer,
+    cards: cardsReducer,
+    cardslist: cardslistReducer,
+    form: formReducer
   }),
   applyMiddleware(thunk)
 );
