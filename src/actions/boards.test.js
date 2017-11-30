5ed43a6a617d53cf1d2efe7d79e2b77c57267ddf
jayzamazing <adrianjaylopez@gmail.com>
2017-11-30 16:51:35 -0500

    Updated actions board tests.

--- src/actions/boards.test.js
+++ src/actions/boards.test.js
@@ -8,6 +8,14 @@ import {seedBoardsCount, seedBoardsSingle, createTitle} from '../testutils/seedd
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+//used to mock getting the auth token from state
+const state = () => {
+  return {
+    auth: {
+      authToken: 'fdr3rweasdfqwearfaere'
+    }
+  }
+}
 let bds;
 describe('findBoardsSuccess', () => {
   beforeEach(() => {
@@ -17,16 +25,16 @@ describe('findBoardsSuccess', () => {
     const boards = {};
     const action = findBoardsSuccess(boards);
     expect(action.type).toEqual(FIND_BOARDS_SUCCESS);
-    expect(action.items).toEqual(boards);
+    expect(action.boards).toEqual(undefined);
   });
   it('Should normalize the data', () => {
     const action = findBoardsSuccess(bds);
     //get array of board keys
-    let keys = Object.keys(action.items.boards);
-    expect(action.items.boards[keys[0]]).toHaveProperty('title');
-    expect(action.items.boards[keys[0]].title).toEqual(bds[0].title);
-    expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-    expect(action.items.boards[keys[0]]._id).toEqual(bds[0]._id);
+    let keys = Object.keys(action.boards);
+    expect(action.boards[keys[0]]).toHaveProperty('title');
+    expect(action.boards[keys[0]].title).toEqual(bds[0].title);
+    expect(action.boards[keys[0]]).toHaveProperty('_id');
+    expect(action.boards[keys[0]]._id).toEqual(bds[0]._id);
   });
 });
 describe('getBoards', () => {
@@ -37,31 +45,35 @@ describe('getBoards', () => {
     const boards = {}
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: bds
+      json() {
+        return bds;
+      }
     }));
     const dispatch = jest.fn();
     //call get boards
-    return getBoards()(dispatch)
+    return getBoards()(dispatch, state)
     .then(() => {
-      expect(fetch).toHaveBeenCalledWith('/boards');
+      expect(fetch).toHaveBeenCalledWith("http://localhost:3030/boards", {"headers": {"Accept": "application/json", "Authorization": "Bearer fdr3rweasdfqwearfaere"}});
       expect(dispatch).toHaveBeenCalledWith(findBoardsSuccess(bds));
     });
   });
   it('should normalize data after findBoardsSuccess is dispatched', () => {
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: bds
+      json() {
+        return bds;
+      }
     }));
     const dispatch = jest.fn();
-    return getBoards()(dispatch)
+    return getBoards()(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       //get array of board keys
-      let keys = Object.keys(action.items.boards);
-      expect(action.items.boards[keys[0]]).toHaveProperty('title');
-      expect(action.items.boards[keys[0]].title).toEqual(bds[0].title);
-      expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-      expect(action.items.boards[keys[0]]._id).toEqual(bds[0]._id);
+      let keys = Object.keys(action.boards);
+      expect(action.boards[keys[0]]).toHaveProperty('title');
+      expect(action.boards[keys[0]].title).toEqual(bds[0].title);
+      expect(action.boards[keys[0]]).toHaveProperty('_id');
+      expect(action.boards[keys[0]]._id).toEqual(bds[0]._id);
     });
   });
 });
@@ -73,16 +85,16 @@ describe('createBoardSuccess', () => {
     const boards = {};
     const action = createBoardSuccess(boards);
     expect(action.type).toEqual(CREATE_BOARD_SUCCESS);
-    expect(action.items).toEqual({ boards: { undefined: {} } });
+    expect(action.boards).toEqual({"undefined": {}});
   });
   it('Should normalize the data', () => {
     const action = createBoardSuccess(bds[0]);
     //get array of board keys
-    let keys = Object.keys(action.items.boards);
-    expect(action.items.boards[keys[0]]).toHaveProperty('title');
-    expect(action.items.boards[keys[0]].title).toEqual(bds[0].title);
-    expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-    expect(action.items.boards[keys[0]]._id).toEqual(bds[0]._id);
+    let keys = Object.keys(action.boards);
+    expect(action.boards[keys[0]]).toHaveProperty('title');
+    expect(action.boards[keys[0]].title).toEqual(bds[0].title);
+    expect(action.boards[keys[0]]).toHaveProperty('_id');
+    expect(action.boards[keys[0]]._id).toEqual(bds[0]._id);
   });
 });
 describe('createBoards', () => {
@@ -97,31 +109,38 @@ describe('createBoards', () => {
     const board = seedBoardsSingle(data.title);
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: board
+      json() {
+        return board;
+      }
     }));
     const dispatch = jest.fn();
     //call create boards
-    return createBoards(data)(dispatch)
+    return createBoards(data)(dispatch, state)
     .then(() => {
-      expect(fetch).toHaveBeenCalledWith('/boards', {'body': data, 'method': 'POST'});
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/boards',
+      {"body": `{\"title\":\"${data.title}\"}`, "headers": {"Accept": "application/json",
+      "Authorization": "Bearer fdr3rweasdfqwearfaere", "Content-Type": "application/json"},
+      "method": "POST"});
       expect(dispatch).toHaveBeenCalledWith(createBoardSuccess(board));
     });
   });
   it('should normalize data after createBoardsSuccess is dispatched', () => {
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: bds
+      json() {
+        return bds;
+      }
     }));
     const dispatch = jest.fn();
-    return createBoards({title: ''})(dispatch)
+    return createBoards({title: ''})(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       //get array of board keys
-      let keys = Object.keys(action.items.boards);
-      expect(action.items.boards[keys[0]]).toHaveProperty('title');
-      expect(action.items.boards[keys[0]].title).toEqual(bds.title);
-      expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-      expect(action.items.boards[keys[0]]._id).toEqual(bds._id);
+      let keys = Object.keys(action.boards);
+      expect(action.boards[keys[0]]).toHaveProperty('title');
+      expect(action.boards[keys[0]].title).toEqual(bds.title);
+      expect(action.boards[keys[0]]).toHaveProperty('_id');
+      expect(action.boards[keys[0]]._id).toEqual(bds._id);
     });
   });
 });
@@ -145,9 +164,9 @@ describe('deleteBoards', () => {
     }));
     const dispatch = jest.fn();
     //call create boards
-    return deleteBoards(1)(dispatch)
+    return deleteBoards(1)(dispatch, state)
     .then(() => {
-      expect(fetch).toHaveBeenCalledWith('/boards/1', {"method": "DELETE"});
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/boards/1', {"headers": {"Authorization": "Bearer fdr3rweasdfqwearfaere"}, "method": "DELETE"});
       expect(dispatch).toHaveBeenCalledWith(deleteBoardSuccess(1));
     });
   });
@@ -157,7 +176,7 @@ describe('deleteBoards', () => {
       id: 1
     }));
     const dispatch = jest.fn();
-    return deleteBoards(1)(dispatch)
+    return deleteBoards(1)(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       expect(action.boardId).toEqual(1);
@@ -172,16 +191,16 @@ describe('updateBoardSuccess', () => {//TODO continue from here
     const boards = {};
     const action = updateBoardSuccess(1, boards);
     expect(action.type).toEqual(UPDATE_BOARD_SUCCESS);
-    expect(action.items).toEqual({ boards: { undefined: {} } });
+    expect(action.boards).toEqual({"1": {"_id": 1}});
   });
   it('Should normalize the data', () => {
     const action = updateBoardSuccess(1, bds);
     //get array of board keys
-    let keys = Object.keys(action.items.boards);
-    expect(action.items.boards[keys[0]]).toHaveProperty('title');
-    expect(action.items.boards[keys[0]].title).toEqual(bds.title);
-    expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-    expect(action.items.boards[keys[0]]._id).toEqual(bds._id);
+    let keys = Object.keys(action.boards);
+    expect(action.boards[keys[0]]).toHaveProperty('title');
+    expect(action.boards[keys[0]].title).toEqual(bds.title);
+    expect(action.boards[keys[0]]).toHaveProperty('_id');
+    expect(action.boards[keys[0]]._id).toEqual(bds._id);
   });
 });
 describe('updateBoards', () => {
@@ -196,32 +215,38 @@ describe('updateBoards', () => {
     const board = seedBoardsSingle(data.title);
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: board
+      json() {
+        return data;
+      }
     }));
     const dispatch = jest.fn();
     //call create boards
-    return updateBoards(1, data)(dispatch)
+    return updateBoards(1, data)(dispatch, state)
     .then(() => {
-      // console.log(board);
-      expect(fetch).toHaveBeenCalledWith('/boards/1', {'body': data, 'method': 'PUT'});
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/boards/1',
+      {"body": `{\"title\":\"${data.title}\"}`, "headers": {"Accept": "application/json",
+      "Authorization": "Bearer fdr3rweasdfqwearfaere", "Content-Type": "application/json"},
+      "method": "PUT"});
       expect(dispatch).toHaveBeenCalledWith(updateBoardSuccess(1, data));
     });
   });
   it('should normalize data after updateBoardsSuccess is dispatched', () => {
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: bds
+      json() {
+        return bds;
+      }
     }));
     const dispatch = jest.fn();
-    return updateBoards(1, bds)(dispatch)
+    return updateBoards(1, bds)(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       //get array of board keys
-      let keys = Object.keys(action.items.boards);
-      expect(action.items.boards[keys[0]]).toHaveProperty('title');
-      expect(action.items.boards[keys[0]].title).toEqual(bds.title);
-      expect(action.items.boards[keys[0]]).toHaveProperty('_id');
-      expect(action.items.boards[keys[0]]._id).toEqual(bds._id);
+      let keys = Object.keys(action.boards);
+      expect(action.boards[keys[0]]).toHaveProperty('title');
+      expect(action.boards[keys[0]].title).toEqual(bds.title);
+      expect(action.boards[keys[0]]).toHaveProperty('_id');
+      expect(action.boards[keys[0]]._id).toEqual(bds._id);
     });
   });
 });
