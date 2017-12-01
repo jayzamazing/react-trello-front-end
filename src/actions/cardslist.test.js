ed09fe3308ccafeb0dc1f83d68964644dd88b0b9
jayzamazing <adrianjaylopez@gmail.com>
2017-12-01 10:44:51 -0500

    Updated actions cardslist tests.

--- src/actions/cardslist.test.js
+++ src/actions/cardslist.test.js
@@ -7,6 +7,14 @@ import {seedCardslistCount, seedCardslistSingle, createTitle} from '../testutils
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+//used to mock getting the auth token from state
+const state = () => {
+  return {
+    auth: {
+      authToken: 'fdr3rweasdfqwearfaere'
+    }
+  }
+}
 let cdls;
 describe('createCardslistSuccess', () => {
   beforeEach(() => {
@@ -16,16 +24,16 @@ describe('createCardslistSuccess', () => {
     const cardslist = {};
     const action = createCardslistSuccess(cardslist);
     expect(action.type).toEqual(CREATE_CARDSLIST_SUCCESS);
-    expect(action.items).toEqual({ cardslist: { undefined: {} } });
+    expect(action.cardslist).toEqual({ undefined: {} });
   });
   it('Should normalize the data', () => {
     const action = createCardslistSuccess(cdls[0]);
     //get array of card keys
-    let keys = Object.keys(action.items.cardslist);
-    expect(action.items.cardslist[keys[0]]).toHaveProperty('title');
-    expect(action.items.cardslist[keys[0]].title).toEqual(cdls[0].title);
-    expect(action.items.cardslist[keys[0]]).toHaveProperty('_id');
-    expect(action.items.cardslist[keys[0]]._id).toEqual(cdls[0]._id);
+    let keys = Object.keys(action.cardslist);
+    expect(action.cardslist[keys[0]]).toHaveProperty('title');
+    expect(action.cardslist[keys[0]].title).toEqual(cdls[0].title);
+    expect(action.cardslist[keys[0]]).toHaveProperty('_id');
+    expect(action.cardslist[keys[0]]._id).toEqual(cdls[0]._id);
   });
 });
 describe('createCardslist', () => {
@@ -40,31 +48,39 @@ describe('createCardslist', () => {
     const card = seedCardslistSingle(2343242341311, data.title);
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: card
+      json() {
+        return card;
+      }
     }));
     const dispatch = jest.fn();
     //call create cardslist
-    return createCardslist(data)(dispatch)
+    return createCardslist(data)(dispatch, state)
     .then(() => {
-      expect(fetch).toHaveBeenCalledWith('/cardslist', {'body': data, 'method': 'POST'});
+      // expect(fetch).toHaveBeenCalledWith('/cardslist', {'body': data, 'method': 'POST'});
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/cardslist',
+      {"body": `{\"title\":\"${data.title}\"}`, "headers": {"Accept": "application/json",
+      "Authorization": "Bearer fdr3rweasdfqwearfaere", "Content-Type": "application/json"},
+      "method": "POST"});
       expect(dispatch).toHaveBeenCalledWith(createCardslistSuccess(card));
     });
   });
   it('should normalize data after createCardslistSuccess is dispatched', () => {
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: cdls
+      json() {
+        return cdls;
+      }
     }));
     const dispatch = jest.fn();
-    return createCardslist({title: ''})(dispatch)
+    return createCardslist({title: ''})(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       //get array of card keys
-      let keys = Object.keys(action.items.cardslist);
-      expect(action.items.cardslist[keys[0]]).toHaveProperty('title');
-      expect(action.items.cardslist[keys[0]].title).toEqual(cdls.title);
-      expect(action.items.cardslist[keys[0]]).toHaveProperty('_id');
-      expect(action.items.cardslist[keys[0]]._id).toEqual(cdls._id);
+      let keys = Object.keys(action.cardslist);
+      expect(action.cardslist[keys[0]]).toHaveProperty('title');
+      expect(action.cardslist[keys[0]].title).toEqual(cdls.title);
+      expect(action.cardslist[keys[0]]).toHaveProperty('_id');
+      expect(action.cardslist[keys[0]]._id).toEqual(cdls._id);
     });
   });
 });
@@ -88,9 +104,11 @@ describe('deleteCardslist', () => {
     }));
     const dispatch = jest.fn();
     //call create cardslist
-    return deleteCardslist(1)(dispatch)
+    return deleteCardslist(1)(dispatch, state)
     .then(() => {
-      expect(fetch).toHaveBeenCalledWith('/cardslist/1', {"method": "DELETE"});
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/cardslist/1',
+      {"headers": {"Authorization": "Bearer fdr3rweasdfqwearfaere"},
+      "method": "DELETE"});
       expect(dispatch).toHaveBeenCalledWith(deleteCardslistSuccess(1));
     });
   });
@@ -100,7 +118,7 @@ describe('deleteCardslist', () => {
       id: 1
     }));
     const dispatch = jest.fn();
-    return deleteCardslist(1)(dispatch)
+    return deleteCardslist(1)(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       expect(action.cardslistId).toEqual(1);
@@ -115,16 +133,16 @@ describe('updateCardslistSuccess', () => {//TODO continue from here
     const cardslist = {};
     const action = updateCardslistSuccess(1, cardslist);
     expect(action.type).toEqual(UPDATE_CARDSLIST_SUCCESS);
-    expect(action.items).toEqual({ cardslist: { undefined: {} } });
+    expect(action.cardslist).toEqual({"1": {"_id": 1}});
   });
   it('Should normalize the data', () => {
     const action = updateCardslistSuccess(1, cdls);
     //get array of card keys
-    let keys = Object.keys(action.items.cardslist);
-    expect(action.items.cardslist[keys[0]]).toHaveProperty('title');
-    expect(action.items.cardslist[keys[0]].title).toEqual(cdls.title);
-    expect(action.items.cardslist[keys[0]]).toHaveProperty('_id');
-    expect(action.items.cardslist[keys[0]]._id).toEqual(cdls._id);
+    let keys = Object.keys(action.cardslist);
+    expect(action.cardslist[keys[0]]).toHaveProperty('title');
+    expect(action.cardslist[keys[0]].title).toEqual(cdls.title);
+    expect(action.cardslist[keys[0]]).toHaveProperty('_id');
+    expect(action.cardslist[keys[0]]._id).toEqual(cdls._id);
   });
 });
 describe('updateCardslist', () => {
@@ -139,32 +157,39 @@ describe('updateCardslist', () => {
     const card = seedCardslistSingle(2343242341311, data.title);
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: card
+      json() {
+        return card;
+      }
     }));
     const dispatch = jest.fn();
     //call create cardslist
-    return updateCardslist(1, data)(dispatch)
+    return updateCardslist(1, data)(dispatch, state)
     .then(() => {
       // console.log(card);
-      expect(fetch).toHaveBeenCalledWith('/cardslist/1', {'body': data, 'method': 'PUT'});
-      expect(dispatch).toHaveBeenCalledWith(updateCardslistSuccess(1, data));
+      expect(fetch).toHaveBeenCalledWith('http://localhost:3030/cardslist/1',
+      {"body": `{\"title\":\"${data.title}\"}`, "headers": {"Accept": "application/json",
+      "Authorization": "Bearer fdr3rweasdfqwearfaere", "Content-Type": "application/json"},
+      "method": "PUT"});
+      expect(dispatch).toHaveBeenCalledWith(updateCardslistSuccess(1, card));
     });
   });
   it('should normalize data after updateCardslistSuccess is dispatched', () => {
     global.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      body: cdls
+      json() {
+        return cdls;
+      }
     }));
     const dispatch = jest.fn();
-    return updateCardslist(1, cdls)(dispatch)
+    return updateCardslist(1, cdls)(dispatch, state)
     .then(() => {
       const action = dispatch.mock.calls[0][0];
       //get array of card keys
-      let keys = Object.keys(action.items.cardslist);
-      expect(action.items.cardslist[keys[0]]).toHaveProperty('title');
-      expect(action.items.cardslist[keys[0]].title).toEqual(cdls.title);
-      expect(action.items.cardslist[keys[0]]).toHaveProperty('_id');
-      expect(action.items.cardslist[keys[0]]._id).toEqual(cdls._id);
+      let keys = Object.keys(action.cardslist);
+      expect(action.cardslist[keys[0]]).toHaveProperty('title');
+      expect(action.cardslist[keys[0]].title).toEqual(cdls.title);
+      expect(action.cardslist[keys[0]]).toHaveProperty('_id');
+      expect(action.cardslist[keys[0]]._id).toEqual(cdls._id);
     });
   });
 });
