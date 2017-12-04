dee091038dfe432d2d55b38d0bf9617cb725e98c
jayzamazing <adrianjaylopez@gmail.com>
2017-12-04 00:21:45 -0500

    Added test for create-cards-form and snapshot.

--- src/components/create-cards-form.test.js
+++ src/components/create-cards-form.test.js
@@ -1,34 +1,34 @@
 'use strict';
 import React from 'react';
-import component, {CreateBoardForm} from './create-board-form';
-import {shallow, mount} from 'enzyme';
 import toJson from 'enzyme-to-json';
+import {shallow} from 'enzyme';
+import {CreateCardsForm} from './create-cards-form';
 
-describe('create-board-form', () => {
+describe('create-cards-form', () => {
   //ensure that it renders without issues
   it('should render', () => {
     const handleSubmit = jest.fn();
-    shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
+    shallow(<CreateCardsForm handleSubmit={handleSubmit}/>);
   });
   //take snapshot for future changes
   it('snapshot', () => {
     const handleSubmit = jest.fn();
-    const wrapper = shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
+    const wrapper = shallow(<CreateCardsForm handleSubmit={handleSubmit}/>);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
   //click on create button and check dispatches
-  it('add board click', () => {
+  it('add cards click', () => {
     const handleSubmit = jest.fn();
-    const wrapper = shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
-    wrapper.find('[name="boardTitle"]').at(0).simulate('change', { target: { value: 'testing' } });
-    wrapper.find('.create-board-input-area').simulate('submit');
+    const wrapper = shallow(<CreateCardsForm handleSubmit={handleSubmit}/>);
+    wrapper.find('[name="cardsTitle"]').at(0).simulate('change', { target: { value: 'testing' } });
+    wrapper.find('.create-cards-btn').simulate('submit');
     expect(handleSubmit).toHaveBeenCalled();
   });
   //click on create button and check dispatches
   it('close button', () => {
     const handleSubmit = jest.fn();
     const closeModal = jest.fn();
-    const wrapper = shallow(<CreateBoardForm closeModal={closeModal} handleSubmit={handleSubmit}/>);
+    const wrapper = shallow(<CreateCardsForm close={closeModal} handleSubmit={handleSubmit}/>);
     wrapper.find('[aria-label="close button"]').simulate('click');
     expect(closeModal).toHaveBeenCalled();
   });
