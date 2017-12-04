b387e46409c5fde4ddf66fe4617926f34acbc95f
jayzamazing <adrianjaylopez@gmail.com>
2017-12-03 23:43:16 -0500

    Added tests for create-board-form and snapshots.

--- src/components/create-board-form.js
+++ src/components/create-board-form.js
@@ -4,14 +4,14 @@ import Input from './input';
 import {required, nonEmpty, isTrimmed} from '../validators';
 
 export function CreateBoardForm(props) {
-  return (<form className="create-board-input-area" onSubmit={props.handleSubmit(values => {
+  return (<form name="createBoardForm" className="create-board-input-area" onSubmit={props.handleSubmit(values => {
       props.onSubmit(values.boardTitle);
     })}>
     <Field component={Input} type="text" name="boardTitle" validate={[required, nonEmpty, isTrimmed]} inputClass="create-board-input" placeholder="Add board title" labelclass="remove"/>
     <button type="button" className="btn btn-default close-modal-btn" aria-label="close button" onClick={() => props.closeModal()}>
       <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
     </button>
-    <button className="create-board-btn btn" type="submit" disabled={props.pristine || props.submitting}>
+    <button className="create-board-btn btn" name="createBoardBtn" type="submit" disabled={props.pristine || props.submitting}>
       Create Board
     </button>
   </form>);
---
+++ src/components/create-board-form.test.js
@@ -0,0 +1,35 @@
+'use strict';
+import React from 'react';
+import component, {CreateBoardForm} from './create-board-form';
+import {shallow, mount} from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+describe('CreateBoardForm', () => {
+  //ensure that it renders without issues
+  it('should render', () => {
+    const handleSubmit = jest.fn();
+    shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
+  });
+  //take snapshot for future changes
+  it('snapshot', () => {
+    const handleSubmit = jest.fn();
+    const wrapper = shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  //click on create button and check dispatches
+  it('add cardslist click', () => {
+    const handleSubmit = jest.fn();
+    const wrapper = shallow(<CreateBoardForm handleSubmit={handleSubmit}/>);
+    wrapper.find('[name="boardTitle"]').at(0).simulate('change', { target: { value: 'testing' } });
+    wrapper.find('.create-board-input-area').simulate('submit');
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+  //click on create button and check dispatches
+  it('close button', () => {
+    const handleSubmit = jest.fn();
+    const closeModal = jest.fn();
+    const wrapper = shallow(<CreateBoardForm closeModal={closeModal} handleSubmit={handleSubmit}/>);
+    wrapper.find('[aria-label="close button"]').simulate('click');
+    expect(closeModal).toHaveBeenCalled();
+  });
+});
