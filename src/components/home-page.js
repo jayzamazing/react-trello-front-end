067d6ef102769d4e0b69de3406d71519b5a270e9
jayzamazing <adrianjaylopez@gmail.com>
2017-12-08 09:41:58 -0500

    Added title to iframe to fix issue with travis.

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -38,7 +38,8 @@ export class HomePage extends React.Component {
           </div>
           <div className="row wrapper-2 video">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/GQuNxPiQWTY?rel=0"
-              frameborder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
+              frameborder="0" gesture="media" allow="encrypted-media" allowFullScreen
+              title="trello walk through"></iframe>
           </div>
         </main>
       </section>
